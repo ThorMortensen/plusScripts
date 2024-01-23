bc45ea61dac7ce08ca3f1ded3665a49ae43c6120
Thor Mortensen <tm@connectedcars.dk>
2024-01-23 11:01:30 +0100

    added spinner

--- workers/device-info-remote-cmd.sh
+++ workers/device-info-remote-cmd.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-# Function to print titles in bold and underlined
-print_title() {
-    echo -e "\033[1m\033[4m$1\033[0m"
-}
-
-#     if [[ $host == "iwg26-v3*" ]]; then echo "YESS" fi;;
-
-# if echo "$(hostname)" | grep -q "^iwg26-v2"; then echo yeeees; fi
-# elif echo "$(hostname)" | grep -q "^iwg26-v3"; then
-#     IFACE=wwan0
-# else
-#     IFACE=unknown
-# fi
 
 # Function to determine the network interface based on hostname
 determine_iface() {
@@ -43,33 +30,15 @@ get_ip_address() {
     fi
 }
 
-get_state_info() {
-    sudo cc-pmcli gpios 
-    # echo "------------------"
-    # sudo cc-pmcli state | tr ',' '\n' | awk -F: '{print $1 ": " $2}'
-    # echo "------------------"
 
-}
-# Main code wrapped in a function for ease of testing
 main() {
-    # print_title "Device type:"
-    # hostname
 
-    # print_title "SW versions:"
-    # sudo cc-hal updatecli versions
-
-    # print_title "Device:"
-    # cc-info
-
-    # print_title "Onomondo addr:"
     local host=$(hostname)
 
     local iface=$(determine_iface $host)
     get_ip_address $iface
 
-#     print_title "State:"
-#     get_state_info
+
 }
 
-# Call the main function
 main
